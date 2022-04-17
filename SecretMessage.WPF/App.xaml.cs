28e3c83e6266c76b40ea9e64828b686e8b8b37db
SingletonSean <singletonsean4@gmail.com>
2022-04-17 14:45:25 -0400

    Add project files.

---
+++ SecretMessage.WPF/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Firebase.Auth;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SecretMessage.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host
+                .CreateDefaultBuilder()
+                .ConfigureServices((context, service) =>
+                {
+                    string firebaseApiKey = context.Configuration.GetValue<string>("FIREBASE_API_KEY");
+
+                    service.AddSingleton(new FirebaseAuthProvider(new FirebaseConfig(firebaseApiKey)));
+
+                    service.AddSingleton<MainWindow>((services) => new MainWindow());
+                })
+                .Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+            
+            base.OnStartup(e);
+        }
+    }
+}
